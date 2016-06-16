4738fcc3606ec36e27954ef966d34888d997671f
Ismael Vayra <ismael.vayra@globant.com>
2016-06-16 17:16:57 -0300

    first commit

---
+++ app/src/main/java/tanoandroidexamples/mvvm_pattern_example/activities/MainActivity.java
@@ -0,0 +1,12 @@
+package tanoandroidexamples.mvvm_pattern_example.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
